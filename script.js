2f4d1c537f481690083003c177a8d2c436582869
Raquel Wetzel <raquelwetzel@gmail.com>
2022-06-14 12:22:39 +0100

    shoes and access pages, css

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 // SINGULAR PRODUCT PAGE IMAGES
-let MainImg=document.getElementById("MainImg")
-let smallImg=document.getElementsByClassName("small-img")
+let SmallImg=document.querySelectorAll(".small-img-group img")
+let MainImg=document.querySelector(".single-mat-image")
 
-smallImg[0].onclick = function(){
-    MainImg.src=smallImg[0].src
-}
-smallImg[1].onclick = function(){
-    MainImg.src=smallImg[1].src
-}
-smallImg[2].onclick = function(){
-    MainImg.src=smallImg[2].src
-}
-smallImg[3].onclick = function(){
-    MainImg.src=smallImg[3].src
-}
+let activeImageSlide = 0
+SmallImg.forEach((item,i)=>{
+    item.addEventListener('click',()=>{
+        SmallImg[activeImageSlide].classList.remove('active');
+        item.classList.add('active');
+        MainImg.innerHTML= "<img src='"+`${item.src}`+"' alt=''/> "
+        activeImageSlide =i;
+    })
+})
+
+
+const sizeBtns =document.querySelectorAll('.size-radio-btn');
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i)=>{
+    item.addEventListener('click', ()=>{
+        sizeBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn=i;
+    })
+})
+
+
+   
+
+
+
+//let SmallImg=document.querySelectorAll(".small-img-group img")
+// let MainImg=document.querySelector(".single-mat-image")
+
+// let activeImageSlide = 0
+// SmallImg.forEach((item,i)=>{
+//     item.addEventListener('click',()=>{
+//         SmallImg[activeImageSlide].classList.remove('active');
+//         item.classList.add('active');
+//         MainImg.innerHTML= "<img src='"+`${item.src}`+"' alt=''/> "
+//         activeImageSlide =i;
+//     })
+// })
